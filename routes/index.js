c057498cad432d2d90c5ba304a903905b6c2fe7a
KirstynNatavio <kirstynatavio@hotmail.com>
2017-11-03 20:47:25 -0400

    Connect Cron with Twilio

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+var cron = require('node-cron');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Tyme' });
+	delayMessage('+13215279114',"IT WOOOOORRRRRKSS");
 });
+
+function delayMessage(number, message) {
+	const accountSid = 'AC65bdbdfbf0837bccd4962a2293745ceb';
+    const authToken = '47d2cd5dc2994c6824db3ea677586b5d';
+    const client = require('twilio')(accountSid, authToken);
+    cron.schedule('5 * * * * *', function() {
+		client.messages
+		.create({
+			to: number,
+			from: '+12012926280',
+			body: message,
+     	})
+     	.then((message) => console.log(message.sid));
+    });
+}
 
 module.exports = router;
